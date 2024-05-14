40851009a1182c6b274c6c6bd2edd3caeaeff4e3
kalamakadanca <kalamakadanca@seznam.cz>
2024-05-14 19:22:37 +0200

    d

--- isaac-slzy.py
+++ isaac-slzy.py
@@ -28,7 +28,7 @@ WIDTH, HEIGHT = 1500, 1000
 clock = pygame.time.Clock()
 
 # Load the character image
-velikost_postavy = 30
+velikost_postavy = 50
 postava = pygame.image.load("pixelovy_isaac_vetsi.png")
 
 # Game loop
