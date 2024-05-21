0bea465d0849505479b1c43ea6f3db31dd7ef1e8
kalamakadanca <kalamakadanca@seznam.cz>
2024-05-22 00:12:48 +0200

    moucha <vypelichana, ale cool>

---
+++ danca_nesahat/panorama.py
@@ -0,0 +1,94 @@
+import pygame, sys
+pygame.init()
+
+# Define color constants
+telova = (255, 186, 141)
+hneda = (85, 33, 0)
+modra_mouchy =(165, 199, 206)
+cerna = (0, 0, 0)
+
+# Create the game window + Load images
+window = pygame.display.set_mode((1000, 600))
+pygame.display.set_caption("ˇIsaacˇ")
+pozadi = pygame.image.load("nakres_dveri.png")
+window.blit(pozadi, (0, 0))
+aktualni_room = 1
+
+""" ENEMIES """
+
+
+
+# Set initial position and dimensions for the character
+rect_x, rect_y = 70, 500
+WIDTH, HEIGHT = 1000, 600
+
+# Create a clock object to control the frame rate
+clock = pygame.time.Clock()
+
+# Load the character and tear image
+velikost_postavy = 57
+postava = pygame.image.load("pixelovy_isaac_vetsi.png")
+importovani_slzy = pygame.image.load("tear.png")
+heart_full = pygame.image.load("srdicka/full_heart.png")
+heart_half = pygame.image.load("srdicka/half_a_heart.png")
+moucha = pygame.image.load("enemies/moucha.png")
+    
+pygame.display.update()
+
+# Game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    keys = pygame.key.get_pressed()
+    # Move the character based on the pressed keys
+    if keys[pygame.K_a]:
+        rect_x -= 5
+    if keys[pygame.K_d]:
+        rect_x += 5
+    if keys[pygame.K_w]:
+        rect_y -= 5
+    if keys[pygame.K_s]:
+        rect_y += 5
+
+    # Vykreslení pozadí
+    window.blit(pozadi, (0, 0))
+
+    # Ensure the character stays within the window boundaries
+    rect_x = max(40, min(rect_x, WIDTH - 101))
+    rect_y = max(40, min(rect_y, HEIGHT - 96))
+
+    # Draw the player health bar
+    prvni_srdicko = window.blit(heart_full, (25, 25))
+    frst_srd = 1
+    druhe_srdicko = window.blit(heart_full, (95, 25))
+    scnd_srd = 1
+    trati_srdicko = window.blit(heart_half, (165, 25))
+    thrd_srd = 1
+
+    # Draw the character and hearts on the window
+    isaac = window.blit(postava, (rect_x, rect_y))
+
+    if isaac.colliderect(pygame.Rect(40, 250, 1, 100)):
+        rect_x += 855
+        print("zapadni")
+    elif isaac.colliderect(pygame.Rect(960,250, 1, 100)):
+        rect_x -= 855
+        print("vychodni")
+    elif isaac.colliderect(pygame.Rect(450,40, 100, 1)):
+        rect_y += 460
+        print("severni")
+    elif isaac.colliderect(pygame.Rect(450,560, 100, 1)):
+        rect_y -= 460
+        print("jizni")
+
+    # Draw the character and hearts on the window
+    isaac = window.blit(postava, (rect_x, rect_y))
+    window.blit(moucha, (500, 300))
+
+
+    # Update the display and control the frame rate
+    pygame.display.update()
+    clock.tick(30)
